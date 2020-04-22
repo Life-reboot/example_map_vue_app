7402a7d458a9af61b6eb8ead56732d012fd6eb78
Peter Jang <peter.x.jang@gmail.com>
2020-04-22 18:50:36 -0500

    Display map using mapbox

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
+    <h1>My map</h1>
+    <div id="map"></div>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+#map {
+  width: 100%;
+  height: 500px;
+}
+</style>
+
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  mounted: function() {
+    mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_API_KEY;
+    var map = new mapboxgl.Map({
+      container: "map", // container id
+      style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+      center: [-87.6298, 41.8781], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  },
+};
 </script>
